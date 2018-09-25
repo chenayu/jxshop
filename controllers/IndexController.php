c1887af2cc517d173fa71bbdd7c6820ec5b93c85
s <xiangyu@czxy.com>
2018-09-25 15:40:28 +0800

    '添加后台'

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -7,4 +7,17 @@ class IndexController
     {
         view('index/index');
     }
+    public function top()
+    {
+        view('index/top');
+    }
+    public function menu()
+    {
+        view('index/menu');
+    }
+    public function main()
+    {
+        view('index/main');
+
+    }
 }
---
+++ libs/Db.php
@@ -0,0 +1,22 @@
+<?php
+namespace libs;
+
+class Db
+{
+    private static $_obj = null;
+    private function __clone(){}
+    private $pdo;
+    private function __construct(){
+        $this->pdo = new \PDO('mysql:host=localhost;dbname=jxshop','root','');
+        $this->pdo->exec('set names utf8');
+    }
+
+    public static function make()
+    {
+        if(self::$_obj===null)
+        {
+            self::$_obj = new self;
+        }
+        return self::$_obj;
+    }
+}
